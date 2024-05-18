e8c3b414c77e26e48ed65e305aa14265383f9300
Inchan kim <kiminchan12341@gmail.com>
2024-05-18 13:53:59 +0900

    test2
    success

--- src/main/java/com/ot/shop/admin/controller/impl/AdminControllerImpl.java
+++ src/main/java/com/ot/shop/admin/controller/impl/AdminControllerImpl.java
@@ -3,5 +3,8 @@ package com.ot.shop.admin.controller.impl;
 import com.ot.shop.admin.controller.AdminController;
 
 public class AdminControllerImpl implements AdminController {
-
+	
+	public String test2() {
+		return "Hello2";
+	}
 }
